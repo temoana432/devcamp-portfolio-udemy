092f55e4aba846b80eb039480b35479637e5d36b
temoana432 <SteveClarke40@hotmail.com>
2022-05-04 09:22:35 -0600

    Added code for copyright generator called SteveCViewTool temporarily to the ApplicationController to test before moving to its own gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,20 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = SteveCViewTool::Renderer.copyright 'Steve Clarke', 'All Rights Reserved'
+    # binding.pry
+  end
+end
+
+module SteveCViewTool
+  class Renderer 
+    def self.copyright name, msg
+      # Use the html copyright symbol
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
