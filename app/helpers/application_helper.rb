225df686b7de5fa23b419889cffdf9aacb4affea
temoana432 <SteveClarke40@hotmail.com>
2022-05-09 11:15:45 -0600

    Code to create a gem accessible from github (but not rubygems.org

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,9 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator
+    DevcampViewTool::Renderer.copyright 'Steve Clarke', 'All Rights  Reserved'
+    # binding.pry
+  end
 end
